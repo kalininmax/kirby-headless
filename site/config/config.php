2e4c995bf7c411449f58677b7e6cb9303548e381
Maksim Kalinin <kalinin.m.a@bk.ru>
2026-04-30 17:59:37 +0300

    Add panel assets

--- site/config/config.php
+++ site/config/config.php
@@ -5,17 +5,18 @@ $baseDir = dirname(__DIR__, 2);
 \Beebmx\KirbyEnv::load($baseDir);
 
 return [
-  'debug' => env('KIRBY_DEBUG', false),
+  'debug'    => env('KIRBY_DEBUG', false),
   'headless' => [
     'globalRoutes' => true,
-    'token' => env('KIRBY_API_TOKEN'),
-    'panel' => [
-      'redirect' => true,
+    'token'        => env('KIRBY_API_TOKEN', 'your-secret-token'),
+    'panel'        => [
+      'redirect'    => true,
       'frontendUrl' => env('FRONTEND_URL', 'https://example.com'),
     ]
   ],
   'panel' => [
     'vue.compiler' => false,
+    'css'          => 'assets/css/custom-panel.css',
   ],
   'routes' => [
     require __DIR__ . '/routes/globals.php',
